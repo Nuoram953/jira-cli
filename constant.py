2efaa38ddf954a97b2a005ede4ba5c178eb87647
Nuoram <antoinemaroun@gmail.com>
2024-07-22 22:57:14 -0400

    Updated structure

---
+++ constant.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Option(Enum):
+    ID = {"full": "--id", "short": "-i", "help": ""}
+    SPRINT ={"full": "--sprint", "short": "-s", "help": ""}
+    PARENT ={"full": "--parent", "short": "-p", "help": ""}
+    WHO = "--who"
+    CREATION = "--creation"
+    TYPE = "--type"
+    WEB = "--web"
+    TIME = "--time"
+    COMMENT = "--comment"
+    PING = "--ping"
+
+    def full(self):
+        return self.value["full"]
+
+    def short(self):
+        return self.value["short"]
+
+    def help(self):
+        return self.value["help"]
+
+
+class ErrorMessage(Enum):
+    WIP = "Not Implemented"
--- main.py
+++ main.py
@@ -3,56 +3,82 @@
 import click
 import inquirer
 
+from constant import Option, ErrorMessage
+
+
 @click.group()
 def cli():
     """CLI tool with multiple commands"""
     pass
 
+
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-@click.option("--parent", default=None, help="")
+@click.option(
+    Option.SPRINT.full(), is_flag=True, default=False, help=Option.SPRINT.help()
+)
+@click.option(
+    Option.PARENT.short(), Option.PARENT.full(), default=None, help=Option.PARENT.help()
+)
 @click.option("--who", default="me", help="Who is the assignee")
 @click.option("--creation", default="me", help="Creation date")
-@click.option("--type", default=None, help="Tpye of the ticket")
-def view_tickets(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option("--type", default=None, help="Tpye of the issue")
+def view_issues(sprint, parent, who, creation, type):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def view_ticket(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+@click.option("--web", default=False, help="Open the issue in your default browser")
+def view_issue(id, web):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def create_ticket(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+def create_issue(id):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def update_ticket(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+def update_issue(id):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-@click.option("--time", help="Id of the ticket")
+@click.option(Option.ID.full(), help=Option.ID.help())
+@click.option("--time", help="Id of the issue")
 @click.option("--comment", help="What did you work on?")
-def add_worklog(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option("--date", help="What did you work on?")
+def add_worklog(id, time, comment, date):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def move_issue(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+def move_issue(id):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def clone_issue(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+def clone_issue(id):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
 
 @cli.command()
-@click.option("--id", help="Id of the ticket")
-def search_issues(id, parent, who, creation, type):
-    raise NotImplementedError("WIP")
+@click.option(Option.ID.full(), help=Option.ID.help())
+def search_issues(id):
+    raise NotImplementedError(ErrorMessage.WIP.value)
 
-if __name__ == '__main__':
+
+@cli.command()
+@click.option(Option.ID.full(), help=Option.ID.help())
+@click.option("--message", help="Id of the issue")
+@click.option("--ping", default=False, help="Add a ping")
+def add_comment(id, message, ping):
+    raise NotImplementedError(ErrorMessage.WIP.value)
+
+
+if __name__ == "__main__":
     cli()
